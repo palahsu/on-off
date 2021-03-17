5fa727fec471cf054a09a41d62f8136cdc4c724c
Palash Ahmed <49250151+palahsu@users.noreply.github.com>
2021-03-17 19:57:04 +0600

    Update Yes or No.py

--- Yes or No.py
+++ Yes or No.py
@@ -1,7 +1,7 @@
 #spam the console! FUN
 
 #define what we want to spam via user input
-Word = input("What would you like to spam the console with? ")
+FUN = input("What would you like to spam the console with? ")
 
 use = input ("Yes or No: ")
 #loop spamming
